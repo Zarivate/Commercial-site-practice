9092f0e48352717efe8815ef8ad6aacc4e889304
Zarivate <Zarivate1@gmail.com>
2022-08-14 23:47:30 -0500

    Managed to retrieve and display data from Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
+import { client } from "../lib/client";
 
-const index = () => {
-  return <div>index</div>;
+const Home = ({ products, bannerData }) => {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>There are many variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
+      <FooterBanner />
+    </>
+  );
 };
 
-export default index;
+// Normally in React useEffect would be used instead to fetch the data from the Sanity API
+// but because this is Next.js, this function is used in place of it
+export const getServerSideProps = async () => {
+  // A Sanity query that says to grab all the products from Sanity dashboard
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  // So whatever is returned gets populated in the function
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
