a68a43411c2b086ea9c5466a4fadd35b79132f9b
Zarivate <Zarivate1@gmail.com>
2022-08-15 23:57:58 -0500

    Worked on homepage some more

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
 
-const Product = () => {
-  return <div>Product</div>;
+// Since the entire product was passed in, it's destructured in the product component
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          {/* If an image property does exist, then the first value in the image array within the product object is returned */}
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,11 @@ const Home = ({ products, bannerData }) => {
         <p>There are many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
